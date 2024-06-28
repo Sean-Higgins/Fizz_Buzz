a769f80e493802b14d0928bf2e267482c138062f
Sean Higgins <seanhiggins639@gmail.com>
2024-06-28 09:35:59 -0700

    Added more comments to make the code easier to understand.

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,27 @@
 use std::io;
 
-const MAX_DIVISORS: usize = 3;
+// These constatns hold the information about the factors to check, as well
+// as their corresponding strings to print if a factor has been found in one of the numbers.
+const MAX_FACTORS: usize = 3;
 
-const DIVISORS: [u32; MAX_DIVISORS] = [3, 5, 7];
-const DIVISOR_STRINGS: [&str; MAX_DIVISORS] = ["Fizz", "Buzz", "Pop"];
+// I was hoping to simplify by making an array of tuples of the form (3, "Fizz"), but sadly that
+// doesn't seem possible. As such, having two corresponding arrays is the best I could manage.
+const FACTORS: [u32; MAX_FACTORS] = [3, 5, 7];
+const FACTOR_STRINGS: [&str; MAX_FACTORS] = ["Fizz", "Buzz", "Pop"];
 
 fn main() {
-    let mut max_nums = String::new();
-
     println!("FizzBuzz");
 
     println!("Please enter the number of trials to perform.");
 
+    let mut max_nums = String::new();
+    
+    // Read the user input for the maximum number of Fizz Buzz trials to perform.
     io::stdin()
         .read_line(&mut max_nums)
         .expect("Failed to read line");
 
+    // Convert the max_nums String to an integer for the program to use.
     let max_nums: u32 = match max_nums.trim().parse() {
         Ok(num) => num,
         Err(_) => {
@@ -24,19 +30,27 @@ fn main() {
         }
     };
 
+    // Check the factors of each number in the range of [1, max_nums]
     for count in 1..max_nums+1 {
+        // This will hold the Fizz and Buzz strings if a number that holds one of our factors is
+        // found.
         let mut result_string = String::new();
 
-        for divisor_count in 0..MAX_DIVISORS {
-
-            if count % DIVISORS[divisor_count] == 0 {
-                result_string.push_str(DIVISOR_STRINGS[divisor_count]);
+        // Check each factor in each number.
+        for factor_count in 0..MAX_FACTORS {
+            // If a factor has been found (i.e., the remainder is 0 when the number is divided by
+            // it), we need to push the corrresponding string into the result_string to print at
+            // the end.
+            if count % FACTORS[factor_count] == 0 {
+                result_string.push_str(FACTOR_STRINGS[factor_count]);
             }
         }
 
-        // Once all of the DIVISORS have been checked, we either print the number if
-        // it is NOT divisible by any of the DIVISORS we've checked, or we print the
-        // divisor string.
+        // Once all of the FACTORS have been checked, we either print the number if
+        // it is NOT divisible by any of the FACTORS we've checked, or we print the
+        // factor string.
+
+        // If there are no other characters in the result_string, we can just print the number.
         if result_string.len() == 0 {
             println!("{}", count);
         } else {
